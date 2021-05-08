61b953883e4151c669b27b7a2bcfb328eeb1e4c7
Djokey <67658015+Djokey@users.noreply.github.com>
2021-05-08 15:59:08 +0700

    0.0.1.0 Groups Update

--- main.py
+++ main.py
@@ -62,6 +62,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.widget_teachers.show()
         self.load_db_teachers()
 
+    # Func for edit database table Teachers
+    def groups_win(self):
+        self.ui.widget_roster.hide()
+        self.ui.widget_groups.show()
+        self.load_db_groups()
+
     # Func for setup all buttons
     def setup_buttons_funcs(self):
         # Func for print docs
@@ -319,6 +325,71 @@ class MainWindow(QtWidgets.QMainWindow):
                     _db.query(_sql)
                     self.load_db_teachers()
 
+        def groups_control_db(type_post):
+            groups_list = self.groups_ui.sAWContent_groups_list.children()
+            _set_doc_warning = 1
+            groups_selected = ''
+            if type_post == 'save':
+                for i in groups_list:
+                    if i.objectName() == 'vL_sAWContent_groups_list':
+                        pass
+                    else:
+                        if i.isChecked():
+                            groups_selected = i.objectName().split('_')[-1]
+                            _set_doc_warning = 0
+                            break
+                        else:
+                            _set_doc_warning = 1
+                if _set_doc_warning:
+                    set_doc_warning("Ошибка (не выбрана запись для изменения)",
+                                    'Сначала выберите запись для изменения.\n\nНажмите на нужную запись, '
+                                    'чтобы выбрать ее, измените ее содержимое, а потом нажмите на кнопку '
+                                    '"Сохранить в выбранную запись"')
+                else:
+                    _db = ARMDataBase()
+                    _sql = "UPDATE groups SET " \
+                           "group_name = '{0}', " \
+                           "class = '{1}', " \
+                           "id_prog = '{2}' " \
+                           "WHERE id_group = '{3}'".format(self.groups_ui.textEdit_groups_name.toPlainText(),
+                                                           self.groups_ui.textEdit_groups_class.toPlainText(),
+                                                           str(self.groups_ui.comboBox_groups_numprog.currentData()),
+                                                           groups_selected)
+                    _db.query(_sql)
+                    self.load_db_groups()
+            elif type_post == 'add':
+                _db = ARMDataBase()
+                _sql = "INSERT INTO groups VALUES(" \
+                       "NULL," \
+                       "'{0}'," \
+                       "'{1}'," \
+                       "'{2}')".format(self.groups_ui.textEdit_groups_class.toPlainText(),
+                                       self.groups_ui.textEdit_groups_name.toPlainText(),
+                                       str(self.groups_ui.comboBox_groups_numprog.currentData()))
+                _db.query(_sql)
+                self.load_db_groups()
+            elif type_post == "del":
+                for i in groups_list:
+                    if i.objectName() == 'vL_sAWContent_groups_list':
+                        pass
+                    else:
+                        if i.isChecked():
+                            groups_selected = i.objectName().split('_')[-1]
+                            _set_doc_warning = 0
+                            break
+                        else:
+                            _set_doc_warning = 1
+                if _set_doc_warning:
+                    set_doc_warning("Ошибка (не выбрана запись для удаления)",
+                                    'Сначала выберите запись для удаления.\n\nНажмите на нужную запись, '
+                                    'чтобы выбрать ее, а потом нажмите на кнопку '
+                                    '"Удалить выбранную запись"')
+                else:
+                    _db = ARMDataBase()
+                    _sql = "DELETE FROM groups WHERE id_group={0}".format(groups_selected)
+                    _db.query(_sql)
+                    self.load_db_groups()
+
         def headers_back():
             self.ui.widget_headers.hide()
             self.ui.widget_roster.show()
@@ -331,6 +402,10 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.widget_teachers.hide()
             self.ui.widget_roster.show()
 
+        def groups_back():
+            self.ui.widget_groups.hide()
+            self.ui.widget_roster.show()
+
         # Setup buts
         self.ui.pushButton_print_notes.clicked.connect(lambda: notes_checked())
         self.ui.pushButton_print_decree.clicked.connect(lambda: decree_checked())
@@ -338,6 +413,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.pushButton_headers_roster.clicked.connect(lambda: self.headers_win())
         self.ui.pushButton_programs_roster.clicked.connect(lambda: self.programs_win())
         self.ui.pushButton_teachers_roster.clicked.connect(lambda: self.teachers_win())
+        self.ui.pushButton_groups_roster.clicked.connect(lambda: self.groups_win())
 
         self.head_ui.pushButton_headers_add.clicked.connect(lambda: headers_control_db('add'))
         self.head_ui.pushButton_headers_save.clicked.connect(lambda: headers_control_db('save'))
@@ -359,6 +435,13 @@ class MainWindow(QtWidgets.QMainWindow):
         self.teach_ui.pushButton_teachers_back.clicked.connect(lambda: teachers_back())
         self.teach_ui.lineEdit_search_teachers.textEdited.connect(
             lambda: self.load_db_teachers(self.teach_ui.lineEdit_search_teachers.text()))
+
+        self.groups_ui.pushButton_groups_add.clicked.connect(lambda: groups_control_db('add'))
+        self.groups_ui.pushButton_groups_save.clicked.connect(lambda: groups_control_db('save'))
+        self.groups_ui.pushButton_groups_delete.clicked.connect(lambda: groups_control_db('del'))
+        self.groups_ui.pushButton_groups_back.clicked.connect(lambda: groups_back())
+        self.groups_ui.lineEdit_search_groups.textEdited.connect(
+            lambda: self.load_db_groups(self.groups_ui.lineEdit_search_groups.text()))
 
     def clear_for_start(self):
         self.ui.widget_headers.hide()
@@ -405,6 +488,10 @@ class MainWindow(QtWidgets.QMainWindow):
         a.setText(self._translate("MainWindow", text))
         return a
 
+    def create_combo_box_el(self, ls, index, text):
+        ls.addItem(text, index)
+        return ls
+
     # Loader database for header
     def load_db_headers(self, search_text=None):
         clear_list(self.head_ui.sAWContent_headers_list.children())
@@ -412,11 +499,14 @@ class MainWindow(QtWidgets.QMainWindow):
         def loader_headers_edits():
             selected_header = ''
             headers_list = self.head_ui.sAWContent_headers_list.children()
-            for head in headers_list:
-                if head.objectName() != 'vL_sAWContent_headers_list':
-                    if head.isChecked():
-                        selected_header = head.objectName().split('_')[-1]
-                        break
+            if len(headers_list) != 2:
+                for head in headers_list:
+                    if head.objectName() != 'vL_sAWContent_headers_list':
+                        if head.isChecked():
+                            selected_header = head.objectName().split('_')[-1]
+                            break
+            else:
+                selected_header = self.head_ui.sAWContent_headers_list.children()[-1].objectName().split('_')[-1]
             _db1 = ARMDataBase()
             _sql1 = "SELECT * FROM headers WHERE id_head=" + selected_header
             _head = _db1.query(_sql1)
@@ -478,11 +568,14 @@ class MainWindow(QtWidgets.QMainWindow):
         def loader_programs_edits():
             selected_program = ''
             programs_list = self.prog_ui.sAWContent_programs_list.children()
-            for prog in programs_list:
-                if prog.objectName() != 'vL_sAWContent_programs_list':
-                    if prog.isChecked():
-                        selected_program = prog.objectName().split('_')[-1]
-                        break
+            if len(programs_list) != 2:
+                for prog in programs_list:
+                    if prog.objectName() != 'vL_sAWContent_programs_list':
+                        if prog.isChecked():
+                            selected_program = prog.objectName().split('_')[-1]
+                            break
+            else:
+                selected_program = self.prog_ui.sAWContent_programs_list.children()[-1].objectName().split('_')[-1]
             _db1 = ARMDataBase()
             _sql1 = "SELECT * FROM programs WHERE id_prog=" + selected_program
             _prog = _db1.query(_sql1)
@@ -532,11 +625,14 @@ class MainWindow(QtWidgets.QMainWindow):
         def loader_teachers_edits():
             selected_teacher = ''
             teachers_list = self.teach_ui.sAWContent_teachers_list.children()
-            for teach in teachers_list:
-                if teach.objectName() != 'vL_sAWContent_teachers_list':
-                    if teach.isChecked():
-                        selected_teacher = teach.objectName().split('_')[-1]
-                        break
+            if len(teachers_list) != 2:
+                for teach in teachers_list:
+                    if teach.objectName() != 'vL_sAWContent_teachers_list':
+                        if teach.isChecked():
+                            selected_teacher = teach.objectName().split('_')[-1]
+                            break
+            else:
+                selected_teacher = self.teach_ui.sAWContent_teachers_list.children()[-1].objectName().split('_')[-1]
             _db1 = ARMDataBase()
             _sql1 = "SELECT * FROM teachers WHERE id_teacher=" + selected_teacher
             _teach = _db1.query(_sql1)
@@ -590,6 +686,78 @@ class MainWindow(QtWidgets.QMainWindow):
                                                 teachs[1] + teachs[5] + teachs[2] + teachs[3] + teachs[4],
                                                 self.teach_ui.sAWContent_teachers_list)
                 teach_but.clicked.connect(lambda: loader_teachers_edits())
+
+    # Loader database for groups
+    def load_db_groups(self, search_text=None):
+        clear_list(self.groups_ui.sAWContent_groups_list.children())
+
+        def loader_groups_edits():
+            selected_group = ''
+            groups_list = self.groups_ui.sAWContent_groups_list.children()
+            if len(groups_list) != 2:
+                for grp in groups_list:
+                    if grp.objectName() != 'vL_sAWContent_groups_list':
+                        if grp.isChecked():
+                            selected_group = grp.objectName().split('_')[-1]
+                            break
+            else:
+                selected_group = self.groups_ui.sAWContent_groups_list.children()[-1].objectName().split('_')[-1]
+            _db1 = ARMDataBase()
+            _sql1 = "SELECT * FROM groups WHERE id_group=" + selected_group
+            _grp = _db1.query(_sql1)
+            self.groups_ui.textEdit_groups_name.setText(_grp[0][2])
+            self.groups_ui.comboBox_groups_numprog.setCurrentIndex(
+                self.groups_ui.comboBox_groups_numprog.findData(_grp[0][3]))
+            if _grp[0][2] is not None or _grp[0][2] != '':
+                self.groups_ui.textEdit_groups_class.setText(_grp[0][1])
+            else:
+                self.groups_ui.textEdit_groups_class.setText('')
+
+        _db = ARMDataBase()
+        _sql = "SELECT * FROM groups"
+        groups = _db.query(_sql)
+        grp_loader = []
+        for i in range(len(groups)):
+            grps = []
+            for h in groups[i]:
+                grps.append(h)
+            grp_loader.append(str(grps[0])[:])
+
+            _db = ARMDataBase()
+            _sql = "SELECT prog_name FROM programs WHERE id_prog=" + str(grps[3])
+            group_prog = _db.query(_sql)
+
+            grps[0] = 'clb_grp_' + str(grps[0])
+            grps[1] = 'Класс: ' + grps[1] + '\n' if grps[1] is not None or grps[1] != '' else ''
+            grps[2] = 'Группа: ' + grps[2] + '\n'
+            grps[3] = 'Программа: ' + group_prog[0][0] + '\n' if group_prog[0][0] is not None or group_prog[0][
+                0] != '' else ''
+            searcher = ''
+            _search_text = search_text
+            for h in grps:
+                if h is not None or h != '':
+                    searcher = searcher + h.lower()
+            if _search_text is not None and _search_text != '':
+                _search_text = search_text.lower()
+                if _search_text in searcher:
+                    grp_but = self.create_list_el(grps[0],
+                                                  grps[2] + grps[3] + grps[1],
+                                                  self.groups_ui.sAWContent_groups_list)
+                    grp_but.clicked.connect(lambda: loader_groups_edits())
+            else:
+                grp_but = self.create_list_el(grps[0],
+                                              grps[2] + grps[3] + grps[1],
+                                              self.groups_ui.sAWContent_groups_list)
+                grp_but.clicked.connect(lambda: loader_groups_edits())
+
+        _db = ARMDataBase()
+        _sql = "SELECT * FROM programs"
+        programs = _db.query(_sql)
+        _programs = []
+
+        self.groups_ui.comboBox_groups_numprog.clear()
+        for prog in programs:
+            self.create_combo_box_el(self.groups_ui.comboBox_groups_numprog, prog[0], str(prog[1]))
 
 
 # Clearing edit list
